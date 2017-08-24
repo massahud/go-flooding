9761c1115821f88b74adb6b83679cf7390001bd7
Geraldo Massahud <massahud@gmail.com>
2017-08-24 13:25:34 +0100

    added sources

---
+++ flooding.go
@@ -0,0 +1,16 @@
+package asdfg
+
+func Flood(terrain *[][]rune, x, y int) {
+	if (y < 0 || y >= len((*terrain)) || x < 0 || x > len((*terrain)[y])) {
+		return
+	}
+	if (*terrain)[y][x] == '.' {
+		(*terrain)[y][x] = 'X'
+		Flood(terrain, x+1, y)
+		Flood(terrain, x-1, y)
+		Flood(terrain, x, y-1)
+		Flood(terrain, x, y+1)
+	}
+}
+
+
